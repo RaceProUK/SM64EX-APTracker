50f95dd41d3b90f258caa3c0b489a0e992c6e989
RaceProUK <RaceProUK@users.noreply.github.com>
2024-03-28 14:30:58 +0000

    Added move rando items to item map

--- pack/scripts/archipelago/itemMap.lua
+++ pack/scripts/archipelago/itemMap.lua
@@ -1,10 +1,25 @@
 ItemMap = {
     [3626000] = { "Stars", "consumable" },
+
     [3626178] = { "KeyBasement", "toggle" },
     [3626179] = { "KeyUpper", "toggle" },
+    
     [3626181] = { "CapWing", "toggle" },
     [3626182] = { "CapMetal", "toggle" },
     [3626183] = { "CapVanish", "toggle" },
+
+    [3626185] = { "DoubleJump", "toggle" },
+    [3626186] = { "TripleJump", "toggle" },
+    [3626187] = { "LongJump", "toggle" },
+    [3626188] = { "BackFlip", "toggle" },
+    [3626189] = { "SideFlip", "toggle" },
+    [3626190] = { "WallKick", "toggle" },
+    [3626191] = { "Dive", "toggle" },
+    [3626192] = { "GroundPound", "toggle" },
+    [3626193] = { "Kick", "toggle" },
+    [3626194] = { "Climb", "toggle" },
+    [3626195] = { "LedgeGrab", "toggle" },
+    
     [3626200] = { "CannonBOB", "toggle" },
     [3626201] = { "CannonWF", "toggle" },
     [3626202] = { "CannonJRB", "toggle" },
